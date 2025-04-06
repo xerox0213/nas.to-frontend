7832bdc6dc3615edd75b58f4c450263a2bf6cc8b
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-06 20:00:34 +0200

    feat(label): add label component

---
+++ src/components/atoms/label/Label.vue
@@ -0,0 +1,15 @@
+<script lang="ts">
+  interface Props {
+    label?: string;
+  }
+</script>
+
+<script setup lang="ts">
+  defineProps<Props>();
+</script>
+
+<template>
+  <label class="text-sm font-medium">
+    <slot>{{ label }}</slot>
+  </label>
+</template>
