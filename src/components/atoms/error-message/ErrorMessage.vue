be88cd9878de7963a1eb28e539bc4fb4168ad84e
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-06 21:00:07 +0200

    feat(error-message): add ErrorMessage component for displaying error messages

---
+++ src/components/atoms/error-message/ErrorMessage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+  defineProps<{
+    error?: string;
+  }>();
+
+  const slots = defineSlots<{
+    default?(): unknown;
+  }>();
+</script>
+
+<template>
+  <span v-if="error || slots.default" class="text-destructive text-sm">
+    <slot>{{ error }}</slot>
+  </span>
+</template>
