1d4ed15316782fe387c974f9132fd5e6bfaef577
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-19 19:29:42 +0200

    chore(deps): add pinia

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,12 @@ import "./style.css";
 import { createApp } from "vue";
 
 import App from "./App.vue";
+import { pinia } from "./plugins/pinia";
 import { router } from "./plugins/router";
 
 const app = createApp(App);
 
+app.use(pinia);
 app.use(router);
 
 app.mount("#app");
