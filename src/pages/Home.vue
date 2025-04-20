f50bc19669aed88fdd0b0d204fb774e01d09246d
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-20 17:01:14 +0200

    feat(router): add Home route with authentication requirement

---
+++ src/pages/Home.vue
@@ -0,0 +1,3 @@
+<template>
+  <h1>Welcome to nas.to</h1>
+</template>
--- src/plugins/router/index.ts
+++ src/plugins/router/index.ts
@@ -3,6 +3,7 @@ import "vue-router";
 import { createRouter, createWebHistory, type RouterOptions } from "vue-router";
 
 import ServerError from "@/pages/errors/500.vue";
+import Home from "@/pages/Home.vue";
 import Login from "@/pages/Login.vue";
 import Register from "@/pages/Register.vue";
 
@@ -30,6 +31,14 @@ const routes: RouterOptions["routes"] = [
     path: "/login",
     meta: {
       requiresAuth: false,
+    },
+  },
+  {
+    component: Home,
+    name: "home",
+    path: "/",
+    meta: {
+      requiresAuth: true,
     },
   },
   {
