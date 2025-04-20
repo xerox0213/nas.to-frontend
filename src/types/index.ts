efd09661fa50b9068f18f3c3d6d0a49d9e4545bb
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-20 16:59:28 +0200

    feat(types): add id field to User type

--- src/types/index.ts
+++ src/types/index.ts
@@ -2,6 +2,7 @@ import type { LoginCredentials } from "@/pages/Login.vue";
 import type { RegistrationCredentials } from "@/pages/Register.vue";
 
 export type User = {
+  id: number;
   name: string;
   email: string;
 };
