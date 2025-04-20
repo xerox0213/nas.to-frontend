62d2a409def43b2feb9d9dc5424c6ae19b1921f8
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-20 16:41:11 +0200

    fix(api): correct XSRF token header and update body type handling

--- src/api/utils.ts
+++ src/api/utils.ts
@@ -4,16 +4,16 @@ export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 export const options = (
   method: RequestInit["method"],
-  body?: RequestInit["body"],
+  body?: object | [],
 ): RequestInit => {
   return {
     method,
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
-      "X-XSRF-TOKEN": Cookies.get("XRF-TOKEN") ?? "",
+      "X-XSRF-TOKEN": Cookies.get("XSRF-TOKEN") ?? "",
     },
-    body,
+    body: JSON.stringify(body),
     credentials: "include",
   };
 };
