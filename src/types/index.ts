e170042543b40f7d7c96548dad812a249d352269
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-20 16:41:51 +0200

    feat(types): define ValidationErrors type for registration validation

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,13 @@
+import type { RegistrationCredentials } from "@/pages/Register.vue";
+
 export type User = {
   name: string;
   email: string;
 };
+
+export type ValidationErrors<T> = {
+  [K in keyof T]?: string[];
+};
+
+export type RegistrationValidationErrors =
+  ValidationErrors<RegistrationCredentials>;
