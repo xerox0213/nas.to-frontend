d07bb481dfe6346b641ba6d63e3820f5001481c4
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-20 13:42:35 +0200

    feat(layout): wrap RouterView in a main container for improved structure

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+  import { RouterView } from "vue-router";
+</script>
 
 <template>
-  <RouterView />
+  <main class="container">
+    <RouterView />
+  </main>
 </template>
