0e9cc623119759c6fdac30420ad0cd832ac4b6d9
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-20 16:56:44 +0200

    feat(api.auth): add login

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -1,4 +1,9 @@
-import { UnauthenticatedError, ValidationError } from "@/errors";
+import {
+  UnauthenticatedError,
+  UnauthorizedError,
+  ValidationError,
+} from "@/errors";
+import type { LoginCredentials } from "@/pages/Login.vue";
 import type { RegistrationCredentials } from "@/pages/Register.vue";
 import type { User } from "@/types";
 
@@ -40,6 +45,26 @@ export const register = async (credentials: RegistrationCredentials) => {
     const error = await res.json();
 
     switch (res.status) {
+      case 422:
+        throw new ValidationError(error.message, error.errors);
+    }
+  }
+};
+
+export const login = async (credentials: LoginCredentials) => {
+  await getCsrf();
+
+  const res = await fetch(
+    `${API_BASE_URL}/api/login`,
+    options("POST", credentials),
+  );
+
+  if (!res.ok) {
+    const error = await res.json();
+
+    switch (res.status) {
+      case 401:
+        throw new UnauthorizedError(error.message);
       case 422:
         throw new ValidationError(error.message, error.errors);
     }
