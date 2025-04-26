7a6006666709db121320252790f8dd448dbdfa11
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-26 15:22:34 +0200

    feat(header): add Header component with navigation and create article button

---
+++ src/components/layout/Header.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+  import Button from "@/components/ui/button/Button.vue";
+</script>
+
+<template>
+  <header class="border-global-border border-b">
+    <div class="container flex items-center justify-between py-2">
+      <RouterLink
+        to="/"
+        aria-label="home"
+        class="bg-primary text-primary-foreground rounded px-2 py-1 text-lg font-bold"
+      >
+        NAS
+      </RouterLink>
+
+      <div>
+        <Button :to="{ name: 'new-article' }" variant="outline">
+          Create Article
+        </Button>
+      </div>
+    </div>
+  </header>
+</template>
