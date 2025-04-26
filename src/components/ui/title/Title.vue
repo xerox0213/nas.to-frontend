1e72b113998c3fd9d0c5e646fec64d970ea334b3
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-26 15:23:58 +0200

    feat(title): create Title component with dynamic heading and styling

---
+++ src/components/ui/title/Title.vue
@@ -0,0 +1,37 @@
+<script lang="ts">
+  import { tv } from "tailwind-variants";
+  import type { HTMLAttributes } from "vue";
+
+  type Heading = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+
+  const title = tv({
+    variants: {
+      heading: {
+        h1: "text-4xl font-extrabold md:text-5xl",
+        h2: "text-3xl font-bold md:text-4xl",
+        h3: "text-2xl font-semibold md:text-3xl",
+        h4: "text-xl font-medium md:text-2xl",
+        h5: "text-lg font-normal md:text-xl",
+        h6: "text-base font-light md:text-lg",
+      },
+    },
+  });
+
+  interface Props {
+    heading?: Heading;
+    class?: HTMLAttributes["class"];
+  }
+</script>
+
+<script setup lang="ts">
+  const props = withDefaults(defineProps<Props>(), {
+    heading: "h1",
+    class: "",
+  });
+</script>
+
+<template>
+  <component :is="heading" :class="title({ heading, class: props.class })">
+    <slot />
+  </component>
+</template>
