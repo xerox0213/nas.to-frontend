dec91addc24492b2d8058b93231fd4271e5962ce
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-04-27 23:05:50 +0200

    feat(api.article): add createArticle

---
+++ src/api/article.ts
@@ -0,0 +1,34 @@
+import { ValidationError } from "yup";
+
+import type { ArticleValues } from "@/components/article/ArticleForm.vue";
+import { CsrfMismatchError, ServerError } from "@/errors/api";
+import type { Article } from "@/types/api";
+import { apiUrl, basicOptions } from "@/utils/api";
+
+export const createArticle = async (
+  values: ArticleValues,
+): Promise<Pick<Article, "id">> => {
+  const formData = new FormData();
+  const keys = Object.keys(values) as Array<keyof typeof values>;
+  keys.forEach((key) => formData.append(key, values[key]));
+
+  const url = apiUrl("api/articles");
+  const options = { ...basicOptions("POST"), body: formData };
+
+  const response = await fetch(url, options);
+
+  if (!response.ok) {
+    const error = await response.json();
+
+    switch (response.status) {
+      case 419:
+        throw new CsrfMismatchError(error.message);
+      case 422:
+        throw new ValidationError(error.message, error.errors);
+      case 500:
+        throw new ServerError(error.message);
+    }
+  }
+
+  return response.json();
+};
