58d17f54ac2cb65ef19c8a333adcfd2bb8895cff
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-02 23:57:58 +0200

    chore(deps): add tanstack query

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import "./style.css";
 
+import { VueQueryPlugin } from "@tanstack/vue-query";
 import { createPinia } from "pinia";
 import { createApp } from "vue";
 
@@ -10,5 +11,6 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(VueQueryPlugin);
 
 app.mount("#app");
