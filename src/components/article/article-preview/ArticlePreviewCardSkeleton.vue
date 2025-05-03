5838a885096429335b61e8ee672466b1d3e5be98
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-03 19:46:14 +0200

    feat(ArticlePreviewCardSkeleton): add skeleton component for article preview loading state

---
+++ src/components/article/article-preview/ArticlePreviewCardSkeleton.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+  import Skeleton from "@/components/ui/skeleton/Skeleton.vue";
+
+  import { articlePreviewTv } from "./ArticlePreviewCard.vue";
+
+  const { root, coverImage, content, authorWrapper, authorDataWrapper } =
+    articlePreviewTv();
+</script>
+
+<template>
+  <div :class="root()">
+    <Skeleton :class="coverImage()" />
+    <div :class="content()">
+      <div :class="authorWrapper()">
+        <Skeleton class="size-8 rounded-full" />
+        <div :class="authorDataWrapper()">
+          <Skeleton class="h-2 w-24 rounded-full" />
+          <Skeleton class="h-2 w-24 rounded-full" />
+        </div>
+      </div>
+      <Skeleton class="h-7 rounded-full" />
+    </div>
+  </div>
+</template>
