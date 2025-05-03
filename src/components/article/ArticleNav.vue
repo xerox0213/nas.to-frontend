2e2040a772b052506d962d2f0ab849d1aa5d97e1
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-03 19:40:05 +0200

    fix(ArticleNav): change button size from large to medium for consistency

--- src/components/article/ArticleNav.vue
+++ src/components/article/ArticleNav.vue
@@ -8,9 +8,9 @@
 
 <template>
   <nav :aria-labelledby="navAccName" class="flex gap-x-2">
-    <Button variant="outline" size="large" to="/">Discover</Button>
+    <Button variant="outline" size="medium" to="/">Discover</Button>
 
-    <Button variant="outline" size="large" to="/following">Following</Button>
+    <Button variant="outline" size="medium" to="/following">Following</Button>
     <span :id="navAccName" hidden>Home content navigation</span>
   </nav>
 </template>
