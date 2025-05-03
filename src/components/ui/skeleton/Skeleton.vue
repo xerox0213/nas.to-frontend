9a5e776748ae7939012f03b38cdaaa53715c71c5
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-03 19:40:22 +0200

    feat(Skeleton): add Skeleton component with Tailwind styling

---
+++ src/components/ui/skeleton/Skeleton.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+  import { tv } from "tailwind-variants";
+  import type { HTMLAttributes } from "vue";
+
+  const skeleton = tv({
+    base: "bg-skeleton animate-pulse cursor-progress",
+  });
+
+  interface Props {
+    class?: HTMLAttributes["class"];
+  }
+</script>
+
+<script setup lang="ts">
+  const props = defineProps<Props>();
+</script>
+
+<template>
+  <div :class="skeleton({ class: props.class })"></div>
+</template>
