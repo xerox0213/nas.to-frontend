23e1742afb0301679604fd38ef5f3060f38cc2f8
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-04 13:07:22 +0200

    feat(following.vue): add initial implementation for Following page with title

---
+++ src/pages/index/following.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+  import Title from "@/components/ui/title/Title.vue";
+</script>
+
+<template>
+  <Title heading="h1" class="mb-5">Following</Title>
+
+  <section></section>
+</template>
