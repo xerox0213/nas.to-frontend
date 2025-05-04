db754c132dc97a2d14ae1877aa9c61c8d6f280fb
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-04 16:39:48 +0200

    feat(article): add ArticleDetailsSkeleton component for loading state

---
+++ src/components/article/article-details/ArticleDetailsSkeleton.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+  import Skeleton from "@/components/ui/skeleton/Skeleton.vue";
+
+  import { articleDetailsTv } from "./ArticleDetails.vue";
+
+  const { coverImage, body, headerWrapper, metaDataWrapper, articleContent } =
+    articleDetailsTv();
+</script>
+
+<template>
+  <div class="cursor-progress">
+    <Skeleton :class="coverImage()" />
+
+    <div :class="body()">
+      <div :class="headerWrapper()">
+        <Skeleton class="size-10 rounded-full" />
+        <div :class="metaDataWrapper()">
+          <Skeleton class="h-2 w-28 rounded-full" />
+          <Skeleton class="h-2 w-28 rounded-full" />
+        </div>
+      </div>
+
+      <div :class="articleContent()">
+        <Skeleton class="h-8 w-full rounded-full" />
+        <Skeleton class="h-24 rounded-sm" />
+      </div>
+    </div>
+  </div>
+</template>
