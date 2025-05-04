81b24c09800c05e5623bd0649bf04d5e055468d4
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-04 16:39:00 +0200

    feat(ui): add Markdown component for rendering markdown content

---
+++ src/components/ui/markdown/Markdown.vue
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { marked } from "marked";
+
+  interface Props {
+    source: string;
+  }
+</script>
+
+<script setup lang="ts">
+  const props = defineProps<Props>();
+
+  const html = marked(props.source);
+</script>
+
+<template>
+  <div v-html="html" />
+</template>
