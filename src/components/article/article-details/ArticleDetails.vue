ffda55905aa821ffda05f5c095ea5f6104d302f8
Nasreddine Boutouil <nasreddine021@gmail.com>
2025-05-04 16:39:42 +0200

    feat(article): create ArticleDetails component for displaying article content

---
+++ src/components/article/article-details/ArticleDetails.vue
@@ -0,0 +1,78 @@
+<script lang="ts">
+  import { tv } from "tailwind-variants";
+
+  import Avatar from "@/components/ui/avatar/Avatar.vue";
+  import Markdown from "@/components/ui/markdown/Markdown.vue";
+  import Title from "@/components/ui/title/Title.vue";
+  import type { ArticleDetails } from "@/types/article";
+
+  export const articleDetailsTv = tv({
+    slots: {
+      coverImage: "h-52 w-full rounded-t-lg object-cover lg:h-96",
+      body: "border-global-border bg-background rounded-b-lg border-x border-b px-4 py-5",
+      headerWrapper: "mb-5 flex gap-x-2",
+      metaDataWrapper: "flex flex-col gap-y-1",
+      dateWrapper: "flex flex-wrap text-sm",
+      articleContent: "space-y-3",
+    },
+  });
+
+  interface Props {
+    article: ArticleDetails;
+  }
+</script>
+
+<script setup lang="ts">
+  defineProps<Props>();
+
+  const {
+    coverImage,
+    body,
+    headerWrapper,
+    metaDataWrapper,
+    dateWrapper,
+    articleContent,
+  } = articleDetailsTv();
+</script>
+
+<template>
+  <article>
+    <a
+      :href="article.cover_image_url"
+      target="_blank"
+      aria-labelledby="acc-name"
+    >
+      <img
+        v-if="article.cover_image_url"
+        :src="article.cover_image_url"
+        alt=""
+        :class="coverImage()"
+      />
+      <span id="acc-name" hidden>Show cover image</span>
+    </a>
+
+    <div :class="body()">
+      <div :class="headerWrapper()">
+        <Avatar
+          :src="article.author.avatar_image_url"
+          size="large"
+          class="shrink-0"
+        />
+        <div :class="metaDataWrapper()">
+          <span>{{ article.author.name }}</span>
+          <div :class="dateWrapper()">
+            <span>{{ article.created_at }}</span>
+            <span>{{ article.updated_at }}</span>
+          </div>
+        </div>
+      </div>
+
+      <div :class="articleContent()">
+        <Title heading="h1">
+          {{ article.title }}
+        </Title>
+        <Markdown :source="article.content" />
+      </div>
+    </div>
+  </article>
+</template>
